305427e67d7d5b4fe2087aa4de737239cf3656d8
Kiseri  Habte <109975015+Kiserry21@users.noreply.github.com>
2022-09-14 22:32:49 -0400

    saving seperated score html and javascript

--- scores.js
+++ scores.js
@@ -1,14 +1,15 @@
 console.log('page-linked')
-var highScoresArray=[]
+var highScoreArray=[]
 highScoreArray=JSON.parse(localStorage.getItem('highscores'))
 console.log(highScoreArray)
 const scoresBoxElement = document.getElementById('scores-box')
 console.log(scoresBoxElement)
-for(let i=0;i<highScoresArray.length;i++)
+for(let i=0;i<highScoreArray.length;i++)
 {
     var highscore=highScoreArray[i]
+    console.log(highscore)
     const item= document.createElement('span')
-item.textContent=highscore
+item.textContent=JSON.stringify(highscore)
 console.log(item)
 scoresBoxElement.appendChild(item)
     
